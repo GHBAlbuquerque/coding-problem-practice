7fe8e5b4b31c61165cdd203fa8e376df400a89b0
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-24 16:48:49 -0300

    balancedParentheses

---
+++ specifics/capitalOne/05.balancedParentheses/BalancedParentheses.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BalancedParentheses {
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (Character parentheses : s.toCharArray()) {
+
+            if (parentheses == '(' || parentheses == '{' || parentheses == '[') {
+                stack.push(parentheses);
+                continue;
+            } else if (stack.isEmpty()) {
+                return false;
+            }else if (parentheses == ')') {
+                if (stack.peek() != '(') return false;
+                stack.pop();
+            } else if (parentheses == '}') {
+                if (stack.peek() != '{') return false;
+                stack.pop();
+            } else if (parentheses == ']') {
+                if (stack.peek() != '[') return false;
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BalancedParentheses.isValid("[[[]]]{}"));
+        System.out.println(BalancedParentheses.isValid("[[[{]]]{}"));
+        System.out.println(BalancedParentheses.isValid("[[[{]]]{"));
+    }
+}
