1787e95ec83d87df33b1cfe1b7c19b8cb84381f6
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-27 15:58:34 -0300

    binarySearch again

---
+++ specifics/capitalOne/08.ImplementBinarySearch/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+
+    public static int search(int[] nums, int target) {
+
+        int lowest = 0;
+        int highest = nums.length - 1;
+
+        while(highest >= lowest) {
+            int index = lowest + (highest - lowest)/2;
+
+            if(target == nums[index]) {
+                return index;
+            } else if (target > nums[index]) {
+                lowest = index + 1;
+            } else {
+                highest = index - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,3,5,9,12};
+        int target = 9;
+
+        System.out.printf("index is %s\n", BinarySearch.search(nums, target));
+
+    }
+}
