6759ab239054fcd523aeb174ac6ebcd6d7fc9332
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-07-30 19:02:09 -0300

    adds code to capitalone folder

---
+++ specifics/capitalOne/11.subArraysWithMaximumSums/SubArraysWithMaximumSums.java
@@ -0,0 +1,23 @@
+public class SubArraysWithMaximumSums {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE; // maximum one found yet
+        int currentSum = 0; // value of the current subarray
+
+        for(int i=0; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            System.out.println(currentSum);
+            //If adding nums[i] makes it worse, just restart the subarray from nums[i]
+
+            maxSum = Math.max(maxSum, currentSum);
+            // then substitute the value if it is greater than the sum
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(SubArraysWithMaximumSums.maxSubArray(nums));
+    }
+}
