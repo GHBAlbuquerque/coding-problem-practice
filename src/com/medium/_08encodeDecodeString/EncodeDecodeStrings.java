466aaa722badb4067cc6665e021d8a8a54335518
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-01 19:53:05 -0300

    stringEncoderPart1

---
+++ src/com/medium/_08encodeDecodeString/EncodeDecodeStrings.java
@@ -0,0 +1,59 @@
+package com.medium._08encodeDecodeString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EncodeDecodeStrings {
+
+    public static String encode(List<String> strs) {
+        if (strs == null || strs.size() == 0) {
+            return "";
+        }
+
+        StringBuilder encoded = new StringBuilder();
+        String separator = "#";
+
+        for(String str : strs) {
+            int length = str.length();
+            encoded.append(length).append(separator).append(str);
+        }
+
+        return encoded.toString();
+    }
+
+    public static List<String> decode(String s) {
+        List<String> strs = new ArrayList<>();
+
+        for(int i=0; i<s.length()-1; i++) {
+            char current = s.charAt(i);
+
+            if(Character.isDigit(current) && s.charAt(i+1) == '#') {
+                    int number = current - '0';
+                    String word = s.substring(i+2, i+2+number);
+                    strs.add(word);
+                    i = i+1+number; // jump ahead considering # and length
+                }
+            }
+
+        return strs;
+    }
+
+    public static void main(String[] args) {
+        List<String> strs = List.of("hello", "world");
+
+        String encoded = EncodeDecodeStrings.encode(strs);
+        List<String> decoded =EncodeDecodeStrings.decode(encoded);
+
+        System.out.println(encoded);
+        System.out.println(decoded);
+
+
+        List<String> strs2 = List.of("", "");
+
+        String encoded2 = EncodeDecodeStrings.encode(strs2);
+        List<String> decoded2 =EncodeDecodeStrings.decode(encoded2);
+
+        System.out.println(encoded2);
+        System.out.println(decoded2);
+    }
+}
