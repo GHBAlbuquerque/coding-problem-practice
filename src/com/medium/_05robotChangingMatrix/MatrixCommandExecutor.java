77832ce2d5ed77d5762fdf109656ba239a6fd4fa
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-03 16:15:16 -0300

    robotChanginMatrix - pt3

--- src/com/medium/_05robotChangingMatrix/MatrixCommandExecutor.java
+++ src/com/medium/_05robotChangingMatrix/MatrixCommandExecutor.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class MatrixCommandExecutor {
 
-    public static int[][] swapRows(int[][] mat, int row1, int row2) {
+    private static int[][] swapRows(int[][] mat, int row1, int row2) {
         int[] temp = mat[row1];
         mat[row1] = mat[row2];
         mat[row2] = temp;
@@ -12,7 +12,7 @@ public class MatrixCommandExecutor {
         return mat;
     }
 
-    public static int[][] swapColumns(int[][] mat, int col1, int col2) {
+    private static int[][] swapColumns(int[][] mat, int col1, int col2) {
 
         for(int i=0; i<mat.length; i++) {
             int temp = mat[i][col1];
@@ -23,7 +23,7 @@ public class MatrixCommandExecutor {
         return mat;
     }
 
-    public static int[][] reverseRow(int[][] mat, int row) {
+    private static int[][] reverseRow(int[][] mat, int row) {
         int rowLength = mat[row].length;
 
         for(int i=0; i<rowLength/2; i++) {
@@ -35,7 +35,7 @@ public class MatrixCommandExecutor {
         return mat;
     }
 
-    public static int[][] reverseColumn(int[][] mat, int col) {
+    private static int[][] reverseColumn(int[][] mat, int col) {
         int colLength = mat.length;
 
         for(int i=0; i<colLength/2; i++) {
@@ -47,11 +47,29 @@ public class MatrixCommandExecutor {
         return mat;
     }
 
-    public static int[][] flipMatrix180(int[][] mat) {
-        int[][] swappedMatrix = new int[mat[0].length][mat.length];
-        System.out.println("flipMatrix180");
-        return mat;
+    private static int[][] transposeMatrix(int[][] mat) {
+        int[][] transposedMatrix = new int[mat[0].length][mat.length]; // don't know if they are square matrixes
+
+        for(int i=0; i< mat.length;i++){
+            for(int j=0; j< mat[i].length;j++){
+                transposedMatrix[j][i] = mat[i][j];
+            }
+        }
+        return transposedMatrix;
     }
+
+
+    private static int[][] flipMatrix90(int[][] mat) {
+        int[][] newMatrix = transposeMatrix(mat);
+        int length = newMatrix.length;
+
+        for(int i=0; i<length; i++){
+            newMatrix = reverseRow(newMatrix, i);
+        }
+
+        return newMatrix;
+    }
+
 
 
     public static int[][] solution(int[][] matrix, String[] commands) {
@@ -62,22 +80,36 @@ public class MatrixCommandExecutor {
             switch(splitString[0]) {
                 case "swapRows": {
                     matrix = swapRows(matrix, Integer.valueOf(splitString[1]), Integer.valueOf(splitString[2]));
+                    /*System.out.println("Swapped rows!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
                     break;
                 }
                 case "swapColumns": {
                     matrix = swapColumns(matrix, Integer.valueOf(splitString[1]), Integer.valueOf(splitString[2]));
+                    /*System.out.println("Swapped cols!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
                     break;
                 }
                 case "reverseRow": {
                     matrix = reverseRow(matrix, Integer.valueOf(splitString[1]));
+                    /*System.out.println("Reversed rows!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
                     break;
                 }
                 case "reverseColumn": {
                     matrix = reverseColumn(matrix, Integer.valueOf(splitString[1]));
+                    /*System.out.println("Reversed cols!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
                     break;
                 }
                 case "flipMatrix180": {
-                    matrix = flipMatrix180(matrix);
+                    matrix = flipMatrix90(matrix);
+                    /*System.out.println("Flipped 90!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
+
+                    matrix = flipMatrix90(matrix);
+                    /*System.out.println("Flipped 90!");
+                    Arrays.stream(matrix).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));*/
                     break;
                 }
             }
@@ -92,15 +124,14 @@ public class MatrixCommandExecutor {
 
         Arrays.stream(matrix1).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));
 
-        MatrixCommandExecutor.solution(matrix1, new String[] {
+        int[][] result = MatrixCommandExecutor.solution(matrix1, new String[] {
                 "swapRows 0 2",
                 "swapColumns 0 2",
                 "reverseRow 1",
-                "reverseColumn 1",
                 "flipMatrix180"
         });
 
-        Arrays.stream(matrix1).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));
+        Arrays.stream(result).toList().stream().forEach(row -> System.out.println(Arrays.toString(row)));
     }
 
 }
