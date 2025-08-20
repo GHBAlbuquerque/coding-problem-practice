2eda72c0c750d224c8c1aa751eef42df0578d7b3
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2025-08-20 17:40:07 -0300

    shuttleMission

--- src/com/easy/_23shuttleMissions/ShuttleMissions.java
+++ src/com/easy/_23shuttleMissions/ShuttleMissions.java
@@ -4,23 +4,25 @@ public class ShuttleMissions {
 
     public static int solution(int[] alpha2beta, int[] beta2alpha, int missions) {
         int time =0;
-        int a2bindex = 0;
+        int a2bindex = 0; // have to check each index separately
         int b2aindex = 0;
 
+        // loop until all missions are done
+        // each missions has one trip going and one coming back
         for (int m =0; m < missions; m++) {
             // ---- Alpha -> Beta ----
             while(a2bindex < alpha2beta.length && alpha2beta[a2bindex] < time) {
                 // need to increment while i don't find a timeslot I can take
                 a2bindex++;
             }
-            time = alpha2beta[a2bindex];
+            time = alpha2beta[a2bindex]; // add going trip
 
             // ---- Beta -> Alpha ----
             while(b2aindex < beta2alpha.length && beta2alpha[b2aindex] < time) {
                 // need to increment while i don't find a timeslot I can take
                 b2aindex++;
             }
-            time = beta2alpha[b2aindex];
+            time = beta2alpha[b2aindex]; // add coming back trip
         }
 
         return time;
